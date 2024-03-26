2e1607ba885716a6a9c9b957f0a592c456888008
Vladimir Lazar <vlazar@redhat.com>
2024-03-26 12:51:48 +0100

    chore(tests): upload script to build extension
    Signed-off-by: Vladimir Lazar <vlazar@redhat.com>

---
+++ tests/playwright/resources/create-extension.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+extensionFolder=bootcextensionlocal
+
+yarn
+yarn build
+mkdir -p $extensionFolder
+cp -r packages/backend/dist/ $extensionFolder/dist
+cp packages/backend/package.json $extensionFolder/
+cp -r packages/backend/media/ $extensionFolder/media
+cp LICENSE $extensionFolder/
+cp packages/backend/icon-dark.png $extensionFolder/
+cp packages/backend/icon-light.png $extensionFolder/
+cp packages/backend/bootable.woff2 $extensionFolder/
+cp README.md $extensionFolder/
+
+# we need to move the folder with extension content into right podman desktop home folder
+mkdir -p tests/playwright/tests/output/bootc-tests-pd/plugins
+cp -r $extensionFolder tests/playwright/tests/output/bootc-tests-pd/plugins
