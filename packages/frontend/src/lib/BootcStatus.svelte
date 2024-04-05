98e77cd94e51a2ee59b31f566b2269174510dadb
Tim deBoer <git@tdeboer.ca>
2024-04-05 09:53:57 -0400

    chore: disk image icon
    We have an icon for disk images now. Renamed the file to be clear, updated the
    image, and changed the two references.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/frontend/src/lib/BootcStatus.svelte
+++ packages/frontend/src/lib/BootcStatus.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 import BootcStatusIcon from './BootcStatusIcon.svelte';
-import BootcIcon from './BootcIcon.svelte';
+import DiskImageIcon from './DiskImageIcon.svelte';
 import type { BootcBuildInfo } from '/@shared/src/models/bootc';
 
 export let object: BootcBuildInfo;
 </script>
 
-<BootcStatusIcon icon="{BootcIcon}" status="{object.status}" />
+<BootcStatusIcon icon="{DiskImageIcon}" status="{object.status}" />
