cfb31ffa76e46efab27a5d70aadb98f0638b46d9
Vladimir Lazar <vlazar@redhat.com>
2024-04-12 13:00:37 +0200

    chore(tests): changing extension folder to custom
    Signed-off-by: Vladimir Lazar <vlazar@redhat.com>

--- tests/playwright/src/bootc-extension.spec.ts
+++ tests/playwright/src/bootc-extension.spec.ts
@@ -48,7 +48,7 @@ beforeEach<RunnerTestContext>(async ctx => {
 });
 
 beforeAll(async () => {
-  pdRunner = new PodmanDesktopRunner({ autoUpdate: false });
+  pdRunner = new PodmanDesktopRunner({ customFolder: 'bootc-tests-pd', autoUpdate: false });
   page = await pdRunner.start();
   pdRunner.setVideoAndTraceName('bootc-e2e');
 
