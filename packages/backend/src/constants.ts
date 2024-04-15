623661729ffd0d63c9e7f1b4218719662d573beb
Tim deBoer <git@tdeboer.ca>
2024-04-15 04:14:04 +0200

    chore: bib latest-1712917745 (#334)
    Latest build of the bootc-image-builder.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/backend/src/constants.ts
+++ packages/backend/src/constants.ts
@@ -18,4 +18,4 @@
 
 // Image related
 export const bootcImageBuilderContainerName = '-bootc-image-builder';
-export const bootcImageBuilderName = 'quay.io/centos-bootc/bootc-image-builder:latest-1712159106';
+export const bootcImageBuilderName = 'quay.io/centos-bootc/bootc-image-builder:latest-1712917745';
