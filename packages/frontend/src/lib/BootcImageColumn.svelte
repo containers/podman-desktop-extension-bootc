a0f2e89276a58f3a114e4fbb4d2d93e9b1cc21e5
Charlie Drage <charlie@charliedrage.com>
2024-06-12 15:41:34 -0400

    feat: add initial light mode for homepage & dashboard
    ### What does this PR do?
    
    Adds initial light mode for the homepage and dashboard (column colour
    changes).
    
    ### Screenshot / video of UI
    
    <!-- If this PR is changing UI, please include
    screenshots or screencasts showing the difference -->
    
    ### What issues does this PR fix or reference?
    
    <!-- Include any related issues from Podman Desktop
    repository (or from another issue tracker). -->
    
    Closes https://github.com/containers/podman-desktop-extension-bootc/issues/539
    
    ### How to test this PR?
    
    Visit homepage and dashboard and see light mode works fine
    
    <!-- Please explain steps to reproduce -->
    
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- packages/frontend/src/lib/BootcImageColumn.svelte
+++ packages/frontend/src/lib/BootcImageColumn.svelte
@@ -4,6 +4,6 @@ import type { BootcBuildInfo } from '/@shared/src/models/bootc';
 export let object: BootcBuildInfo;
 </script>
 
-<div class="text-sm text-gray-300">
+<div class="text-sm text-[var(--pd-table-body-text-highlight)]">
   {object.image}:{object.tag}
 </div>
