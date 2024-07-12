4d7f17b35745d8e194a030cc178654481c61aa40
Tim deBoer <git@tdeboer.ca>
2024-07-12 13:02:55 -0400

    chore: font size and consistency (#626)
    Match up with the new font sizes / consistency as Podman Desktop:
    - Pick up the latest svelte-ui (changes to input/button font).
    - Set the same font sizes as Podman Desktop.
    - Set the default font size in index.html.
    - Make equivalent size changes in Homepage and Build page - mostly
      removing text-sm or moving from -xs to -sm.
    
    Fixes #625.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/frontend/src/lib/BootcFolderColumn.svelte
+++ packages/frontend/src/lib/BootcFolderColumn.svelte
@@ -5,7 +5,7 @@ import type { BootcBuildInfo } from '/@shared/src/models/bootc';
 export let object: BootcBuildInfo;
 </script>
 
-<div class="text-sm text-[var(--pd-table-body-text)] overflow-hidden text-ellipsis">
+<div class="text-[var(--pd-table-body-text)] overflow-hidden text-ellipsis">
   <Link folder="{object.folder}">
     {object.folder}
   </Link>
