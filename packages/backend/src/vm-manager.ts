4a7cfd4b0f9dd5ddc4b5a03dc8a12d10fb62717e
Charlie Drage <charlie@charliedrage.com>
2024-10-23 11:52:20 -0400

    refactor: update vm manager + code suggestions
    ### What does this PR do?
    
    * Updates the VM manager so that it is more modular
    * Fixes suggested code changes
    * Updates DiskImageDetailsVirtualMachine check for websocket with a
      timeout.
    
    ### Screenshot / video of UI
    
    <!-- If this PR is changing UI, please include
    screenshots or screencasts showing the difference -->
    
    N/A
    
    ### What issues does this PR fix or reference?
    
    <!-- Include any related issues from Podman Desktop
    repository (or from another issue tracker). -->
    
    Closes https://github.com/containers/podman-desktop-extension-bootc/issues/953
    
    ### How to test this PR?
    
    <!-- Please explain steps to reproduce -->
    
    Everything should work like normal launching a VM for arm or amd64
    
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- packages/backend/src/vm-manager.ts
+++ packages/backend/src/vm-manager.ts
@@ -37,16 +37,18 @@ const memorySize = '4G';
 const websocketPort = '45252';
 const rawImageLocation = 'image/disk.raw';
 
-export default class VMManager {
-  private build: BootcBuildInfo;
+// Abstract base class
+export abstract class VMManagerBase {
+  protected build: BootcBuildInfo;
 
-  // Only values needed is the location of the VM file as well as the architecture of the image that
-  // will be used.
-  constructor(build?: BootcBuildInfo) {
-    this.build = build!;
+  constructor(build: BootcBuildInfo) {
+    this.build = build;
   }
 
-  // Launch the VM by generating the appropriate QEMU command and then launching it with process.exec
+  public abstract checkVMLaunchPrereqs(): Promise<string | undefined>;
+
+  protected abstract generateLaunchCommand(diskImage: string): string[];
+
   public async launchVM(): Promise<void> {
     const diskImage = this.getDiskImagePath();
 
@@ -65,101 +67,38 @@ export default class VMManager {
 
       await extensionApi.process.exec('sh', ['-c', `${command.join(' ')}`]);
     } catch (e) {
-      this.handleError(e);
+      handleStdError(e);
     }
   }
 
-  // We only support running one VM at at a time, so we kill the process by reading the pid from the universal pid file we use.
-  public async stopCurrentVM(): Promise<void> {
-    try {
-      await extensionApi.process.exec('sh', ['-c', `kill -9 \`cat ${pidFile}\``]);
-    } catch (e) {
-      // Ignore if it contains 'No such process' as that means the VM is already stopped / not running.
-      if (e instanceof Error && 'stderr' in e && typeof e.stderr === 'string' && e.stderr.includes('No such process')) {
-        return;
-      }
-      this.handleError(e);
-    }
+  protected getDiskImagePath(): string {
+    return path.join(this.build.folder, rawImageLocation);
   }
+}
 
-  // Prerequisite checks before launching the VM which includes checking if QEMU is installed as well as other OS specific checks.
+// Mac ARM VM Manager
+class MacArmNativeVMManager extends VMManagerBase {
   public async checkVMLaunchPrereqs(): Promise<string | undefined> {
     const diskImage = this.getDiskImagePath();
     if (!fs.existsSync(diskImage)) {
       return `Raw disk image not found at ${diskImage}. Please build a .raw disk image first.`;
     }
 
-    if (!this.isArchitectureSupported()) {
+    if (this.build.arch !== 'arm64') {
       return `Unsupported architecture: ${this.build.arch}`;
     }
 
-    // Future support for Mac Intel, Linux ARM, Linux X86 and Windows ARM, Windows X86 to be added here.
-    if (isMac() && isArm()) {
-      return this.checkMacPrereqs();
-    } else {
-      return 'Unsupported OS. Only MacOS Silicon is supported.';
-    }
-  }
-
-  private getDiskImagePath(): string {
-    return path.join(this.build.folder, rawImageLocation);
-  }
-
-  private isArchitectureSupported(): boolean {
-    return this.build.arch === 'amd64' || this.build.arch === 'arm64';
-  }
-
-  private checkMacPrereqs(): string | undefined {
     const installDisclaimer = 'Please install qemu via our installation document';
-    if (!fs.existsSync(macQemuX86Binary)) {
-      return `QEMU x86 binary not found at ${macQemuX86Binary}. ${installDisclaimer}`;
-    }
-    if (this.build.arch === 'arm64' && !fs.existsSync(macQemuArm64Binary)) {
+    if (!fs.existsSync(macQemuArm64Binary)) {
       return `QEMU arm64 binary not found at ${macQemuArm64Binary}. ${installDisclaimer}`;
     }
-    if (this.build.arch === 'arm64' && !fs.existsSync(macQemuArm64Edk2)) {
+    if (!fs.existsSync(macQemuArm64Edk2)) {
       return `QEMU arm64 edk2-aarch64-code.fd file not found at ${macQemuArm64Edk2}. ${installDisclaimer}`;
     }
     return undefined;
   }
 
-  // Supported: MacOS Silicon
-  // Unsupported: MacOS Intel, Linux, Windows
-  private generateLaunchCommand(diskImage: string): string[] {
-    // Future support for Mac Intel, Linux ARM, Linux X86 and Windows ARM, Windows X86 to be added here.
-    if (isMac() && isArm()) {
-      switch (this.build.arch) {
-        case 'amd64':
-          return this.generateMacX86Command(diskImage);
-        case 'arm64':
-          return this.generateMacArm64Command(diskImage);
-      }
-    }
-    return [];
-  }
-
-  private generateMacX86Command(diskImage: string): string[] {
-    return [
-      macQemuX86Binary,
-      '-m',
-      memorySize,
-      '-nographic',
-      '-cpu',
-      'Broadwell-v4',
-      '-pidfile',
-      pidFile,
-      '-serial',
-      `websocket:127.0.0.1:${websocketPort},server,nowait`,
-      '-netdev',
-      `user,id=mynet0,${hostForwarding}`,
-      '-device',
-      'e1000,netdev=mynet0',
-      '-snapshot',
-      diskImage,
-    ];
-  }
-
-  private generateMacArm64Command(diskImage: string): string[] {
+  protected generateLaunchCommand(diskImage: string): string[] {
     return [
       macQemuArm64Binary,
       '-m',
@@ -187,14 +126,85 @@ export default class VMManager {
       diskImage,
     ];
   }
+}
 
-  // When running process.exec we should TRY and get stderr which it outputs (sometimes) so we do not get an "exit code 1" error with
-  // no information.
-  private handleError(e: unknown): void {
-    if (e instanceof Error && 'stderr' in e) {
-      throw new Error(typeof e.stderr === 'string' ? e.stderr : 'Unknown error');
-    } else {
-      throw new Error('Unknown error');
+// Mac ARM running x86 images VM Manager
+class MacArmX86VMManager extends VMManagerBase {
+  public async checkVMLaunchPrereqs(): Promise<string | undefined> {
+    const diskImage = this.getDiskImagePath();
+    if (!fs.existsSync(diskImage)) {
+      return `Raw disk image not found at ${diskImage}. Please build a .raw disk image first.`;
     }
+
+    if (this.build.arch !== 'amd64') {
+      return `Unsupported architecture: ${this.build.arch}`;
+    }
+
+    const installDisclaimer = 'Please install qemu via our installation document';
+    if (!fs.existsSync(macQemuX86Binary)) {
+      return `QEMU x86 binary not found at ${macQemuX86Binary}. ${installDisclaimer}`;
+    }
+    return undefined;
+  }
+
+  protected generateLaunchCommand(diskImage: string): string[] {
+    return [
+      macQemuX86Binary,
+      '-m',
+      memorySize,
+      '-nographic',
+      '-cpu',
+      'qemu64',
+      '-machine',
+      'q35',
+      '-accel',
+      'hvf',
+      '-smp',
+      '4',
+      '-serial',
+      `websocket:127.0.0.1:${websocketPort},server,nowait`,
+      '-pidfile',
+      pidFile,
+      '-netdev',
+      `user,id=usernet,${hostForwarding}`,
+      '-device',
+      'e1000,netdev=usernet',
+      '-snapshot',
+      diskImage,
+    ];
+  }
+}
+
+// Factory function to create the appropriate VM Manager
+export function createVMManager(build: BootcBuildInfo): VMManagerBase {
+  // Only thing that we support is Mac M1 at the moment
+  if (isMac() && isArm()) {
+    if (build.arch === 'arm64') {
+      return new MacArmNativeVMManager(build);
+    } else if (build.arch === 'amd64') {
+      return new MacArmX86VMManager(build);
+    }
+  }
+  throw new Error('Unsupported OS or architecture');
+}
+
+// Function to stop the current VM
+export async function stopCurrentVM(): Promise<void> {
+  try {
+    await extensionApi.process.exec('sh', ['-c', `kill -9 \`cat ${pidFile}\``]);
+  } catch (e) {
+    if (e instanceof Error && 'stderr' in e && typeof e.stderr === 'string' && e.stderr.includes('No such process')) {
+      return;
+    }
+    handleStdError(e);
+  }
+}
+
+// Error handling function
+function handleStdError(e: unknown): void {
+  if (e instanceof Error && 'stderr' in e) {
+    throw new Error(typeof e.stderr === 'string' ? e.stderr : 'Unknown error');
+  } else {
+    throw new Error('Unknown error');
   }
 }
